306ee83d22368bc48521eb4096a6c73179efe03c
ishiharay15 <ishiharay15@gmail.com>
2017-08-01 12:33:50 -0400

    Add files via upload

---
+++ merry_faces/code/blink.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+for ((a=1; a = 1 ; a))
+do
+  rosrun baxter_examples xdisplay_image.py --file=final-0006.png
+  rosrun baxter_examples xdisplay_image.py --file=final-0008.png
+  rosrun baxter_examples xdisplay_image.py --file=final-0010.png
+  rosrun baxter_examples xdisplay_image.py --file=final-0012.png
+  rosrun baxter_examples xdisplay_image.py --file=final-0014.png
+  rosrun baxter_examples xdisplay_image.py --file=final-0016.png
+
+done
